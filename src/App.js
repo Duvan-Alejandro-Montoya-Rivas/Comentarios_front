e6dac9d78b695a053631ee0930715f7bb2731883
Duvan Montoya <dubis310@gmail.com>
2022-12-15 14:02:59 -0500

    Comienzo para aprender react como fuerte de desarrollo para front-end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Testimonio from './componentes/Testimonios';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Comentarios ramdons que escribo</h1>
+        <Testimonio 
+        nombre='Emma bostian'
+        pais='Suecia'
+        imagen='1'
+        cargo='Ingeniera en software'
+        empresa='Spotify'
+        testimonio='lisudddddddddddddddddddddddfluibalsiubbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbddsldiuadbashfb'
+        />
+        <Testimonio 
+        nombre='Emanuel Gutieerez'
+        pais='Holanda'
+        imagen='2'
+        cargo='Ingeniero de sistemas'
+        empresa='Tesla'
+        testimonio='eriuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuusvbñubc clzisbc kzcb z'
+        />
+        <Testimonio 
+        nombre='Jose Kottlin'
+        pais='Andres ramierez'
+        imagen='3'
+        cargo='Ingeniero en software'
+        empresa='IBM'
+        testimonio='lirfgbasñoubc asñcuga duiahs ld8agñcihad89 gasd baskjñ dgashdvba sdgliu asdjasjdagduas di asduylasbdi'
+        />
+      </div>
     </div>
   );
 }
